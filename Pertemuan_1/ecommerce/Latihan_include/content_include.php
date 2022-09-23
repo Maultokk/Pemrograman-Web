5705ab72156af4ea71a2404e9a45c28d7513ae3f
Maultokk <wahyumaulana2245@gmail.com>
2022-09-23 11:00:12 +0700

    tugas include dan require

---
+++ Pertemuan_1/ecommerce/Latihan_include/content_include.php
@@ -0,0 +1,7 @@
+<div class="content">
+    <br/>
+    home / About Us 
+    <h1>Tentang saya</1>
+    <p>Nama saya Maulana, dari SMK N 5 SURAKARTA.</p>
+
+</div>
---
+++ Pertemuan_1/ecommerce/Latihan_require/header_require.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Latihan require</title>
+</head>
+<body>
+    <div class="header">
+        <h2>Mengenal require</h2>
+        <hr/>
+    </div>
